a0bb4aa622e157c383a767d012cf05aea5434312
fison <1805696978@qq.com>
2021-01-02 17:02:23 +0800

    修改项目名

--- network/http/src/main/java/com/wsq/network/http/Http.java
+++ network/http/src/main/java/com/wsq/network/http/Http.java
@@ -1,10 +1,30 @@
 package com.wsq.network.http;
 
+import com.wsq.util.common.ConfigUtils;
+
+import static com.wsq.network.http.Executor.getExecutor;
+
 public class Http {
-    private static final int DEFAULT_TIMEOUT = 60 * 1000;
-    private static final boolean DEFAULT_LOG = true;
-    private static final int DEFAULT_RETRIES = 0;
-    private static Executor DEFAULT_EXECUTOR = new Executor(DEFAULT_TIMEOUT, DEFAULT_LOG, DEFAULT_RETRIES);
+    public static final boolean DEFAULT_LOG = ConfigUtils.getBooleanDefault("http.enableLog",true);
+    public static final int DEFAULT_RETRIES = ConfigUtils.getIntDefault("http.retries", 0);
+    public static final long DEFAULT_TIMEOUT = ConfigUtils.getLongDefault("http.timeout", 60 * 1000L);
+
+    private static final Executor DEFAULT_EXECUTOR = new Executor(DEFAULT_LOG, DEFAULT_RETRIES, DEFAULT_TIMEOUT);
+
+    // 默认开启日志，调用该方法可以关闭http相关日志
+    public static Executor disableLog() {
+        return getExecutor(false, DEFAULT_RETRIES, DEFAULT_TIMEOUT);
+    }
+
+    // 默认不重试，调用该方法可以指定重试次数
+    public static Executor retries(int retries) {
+        return getExecutor(DEFAULT_LOG, retries, DEFAULT_TIMEOUT);
+    }
+
+    // 默认超时时间1分钟，调用该方法可以指定超时时间
+    public static Executor timeout(long timeout) {
+        return getExecutor(DEFAULT_LOG, DEFAULT_RETRIES, timeout);
+    }
 
 
 }
---
+++ network/http/src/test/java/com.wsq.network.http/HttpTest.java
@@ -0,0 +1,11 @@
+package com.wsq.network.http;
+
+import org.junit.Test;
+
+public class HttpTest {
+
+    @Test
+    public void httpTest() {
+        Http.disableLog()
+    }
+}
