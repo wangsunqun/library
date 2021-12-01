673dc2adee871e973b3f370dae4dc05f02a7195a
S4545 <wangsunqun@corp.netease.com>
2021-12-01 15:56:57 +0800

    1

---
+++ gateway/shenyu/src/main/java/com/wsq/library/shenyu/ShenYuApplication.java
@@ -0,0 +1,13 @@
+package com.wsq.library.shenyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenYuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShenYuApplication.class, args);
+    }
+
+}
---
+++ gateway/shenyu/src/test/java/com/wsq/library/shenyu/ShenYuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsq.library.shenyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShenYuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
