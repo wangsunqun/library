03ed9afb50d6df99b92bab9376c58648fa880b56
S4545 <wangsunqun@corp.netease.com>
2021-12-31 14:23:14 +0800

    1

--- algorithm/src/main/java/com/wsq/library/algorithm/link/未完成_K个一组翻转链表.java
+++ algorithm/src/main/java/com/wsq/library/algorithm/link/未完成_K个一组翻转链表.java
@@ -1,10 +1,12 @@
 package com.wsq.library.algorithm.link;
 
 import com.alibaba.fastjson.JSON;
+import lombok.Data;
 
 import java.util.ArrayList;
 
 public class 未完成_K个一组翻转链表 {
+    @Data
     private static class ListNode {
         int val;
         ListNode next;
@@ -23,65 +25,57 @@ public class 未完成_K个一组翻转链表 {
     }
 
     public static ListNode reverseKGroup(ListNode head, int k) {
-        ListNode pre = new ListNode(0, head);
-        ListNode last = pre;
+        ListNode pre = null;
 
         while (head != null) {
-            ListNode tmp = head;
+            ListNode last = head;
             ListNode end = getEnd(head, k);
+            if (pre == null) pre = end;
+
             if (end == null) break;
 
-            ListNode tmp2 = end.next;
-
-            if (end.next != null) {
-                ListNode listNode = reverseList(head, end);
-                last.next = listNode;
-                last = tmp;
-                head = tmp2;
-                head.next = tmp2;
-            } else {
-                last.next = tmp;
-            }
+            head = reverseList(head, end);
+            last.next = head;
         }
 
-        return pre.next;
+        return pre;
     }
 
     private static ListNode getEnd(ListNode head, int k) {
         while (head != null) {
-            if (k == 0) {
+            if (--k == 0) {
                 return head;
             }
 
             head = head.next;
-            k--;
         }
 
         return head;
     }
 
     private static ListNode reverseList(ListNode head, ListNode end) {
-        ListNode tmp = head;
-        head = head.next;
+        ListNode tmp = null;
 
         while (head != null) {
+            if (tmp == end) break;
+
             ListNode tmp2 = head.next;
             head.next = tmp;
             tmp = head;
             head = tmp2;
-
-            if (tmp == end) break;
         }
 
-        return tmp;
+        return head;
     }
 
     public static void main(String[] args) {
-        ListNode node3 = new ListNode(3, null);
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
         ListNode node2 = new ListNode(2, node3);
         ListNode node1 = new ListNode(1, node2);
 
-        final ListNode listNode = reverseKGroup(node1, 2);
+        ListNode listNode = reverseKGroup(node1, 2);
         System.out.println(JSON.toJSONString(listNode));
     }
 }
